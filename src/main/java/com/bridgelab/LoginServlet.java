1873f3f9e77c72eb7873a04aea96c1e5afa13fde
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-30 17:06:42 +0530

    Added: Simple Login With Predefined User

---
+++ src/main/java/com/bridgelab/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.bridgelab;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        description = "Login Servlet testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = { @WebInitParam(name="user",value="Deepak"),
+                @WebInitParam(name="password",value="Patil")
+        }
+)
+
+public class LoginServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+                String user = req.getParameter("user");
+                String pwd = req.getParameter("pwd");
+
+                String userID = getServletConfig().getInitParameter("user");
+                String password = getServletConfig().getInitParameter("password");
+
+                if (userID.equals(user) && password.equals(pwd)){
+                        req.setAttribute("user",user);
+                        req.getRequestDispatcher("LoginSuccess.jsp").forward(req,response);
+                } else {
+                        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+                        PrintWriter out = response.getWriter();
+                        out.println("<font color=red>Either user name or password is wrong.</font>");
+                        rd.include(req,response);
+                }
+        }
+}
